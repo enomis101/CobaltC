a29ab17399100bf88327908fa644c825b41b462a
enomis101 <simone.alterni@gmail.com>
2025-04-21 00:59:30 +0200

    Implementing Tacky Printer

---
+++ tacky/include/tacky/tacky_printer.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "tacky/tacky_ast.h"
+#include <sstream>
+#include <unordered_map>
+
+namespace tacky {
+
+class PrinterVisitor : public TackyVisitor {
+public:
+    PrinterVisitor();
+
+    // Generate DOT file from the TackyAST
+    void generate_dot_file(const std::string& filename, TackyAST& ast);
+
+    // Implementation of visitor interface methods
+    void visit(Identifier& node) override;
+    void visit(ComplementOperator& node) override;
+    void visit(NegateOperator& node) override;
+    void visit(UnaryExpression& node) override;
+    void visit(ConstantExpression& node) override;
+    void visit(ReturnStatement& node) override;
+    void visit(Function& node) override;
+    void visit(Program& node) override;
+
+private:
+    // Get or assign a unique ID for each node
+    int get_node_id(const TackyAST* node);
+
+    int m_node_count;                                     // Counter for generating unique node IDs
+    std::unordered_map<const TackyAST*, int> m_node_ids; // Maps TackyAST nodes to their unique IDs
+    std::stringstream m_dot_content;                      // Buffer for dot file content
+};
+
+} // namespace tacky
---
+++ tacky/src/tacky_printer.cpp
@@ -0,0 +1,127 @@
+#include "tacky/tacky_printer.h"
+#include <fstream>
+#include <string>
+
+using namespace tacky;
+
+PrinterVisitor::PrinterVisitor()
+    : m_node_count(0)
+{
+}
+
+void PrinterVisitor::generate_dot_file(const std::string& filename, TackyAST& ast)
+{
+    // Reset state for new file generation
+    m_node_count = 0;
+    m_node_ids.clear();
+    m_dot_content.str("");
+
+    // Start DOT file with digraph definition
+    m_dot_content << "digraph TackyAST {\n";
+    m_dot_content << "  node [shape=box, fontname=\"Arial\", fontsize=10];\n";
+
+    // Visit the TackyAST to build the DOT representation
+    ast.accept(*this);
+
+    // Close the digraph
+    m_dot_content << "}\n";
+
+    // Write to file
+    std::ofstream out_file(filename);
+    if (out_file.is_open()) {
+        out_file << m_dot_content.str();
+        out_file.close();
+    }
+}
+
+void PrinterVisitor::visit(Identifier& node)
+{
+    int id = get_node_id(&node);
+    m_dot_content << "  node" << id << " [label=\"Identifier\\nname: " << node.name << "\"];\n";
+}
+
+void PrinterVisitor::visit(ComplementOperator& node)
+{
+    int id = get_node_id(&node);
+    m_dot_content << "  node" << id << " [label=\"ComplementOperator\"];\n";
+}
+
+void PrinterVisitor::visit(NegateOperator& node)
+{
+    int id = get_node_id(&node);
+    m_dot_content << "  node" << id << " [label=\"NegateOperator\"];\n";
+}
+
+void PrinterVisitor::visit(UnaryExpression& node)
+{
+    int id = get_node_id(&node);
+    m_dot_content << "  node" << id << " [label=\"UnaryExpression\"];\n";
+
+    if (node.unary_operator) {
+        node.unary_operator->accept(*this);
+        m_dot_content << "  node" << id << " -> node" << get_node_id(node.unary_operator.get())
+                      << " [label=\"unary_operator\"];\n";
+    }
+
+    if (node.expression) {
+        node.expression->accept(*this);
+        m_dot_content << "  node" << id << " -> node" << get_node_id(node.expression.get())
+                      << " [label=\"expression\"];\n";
+    }
+}
+
+void PrinterVisitor::visit(ConstantExpression& node)
+{
+    int id = get_node_id(&node);
+    m_dot_content << "  node" << id << " [label=\"ConstantExpression\\nvalue: " << node.value << "\"];\n";
+}
+
+void PrinterVisitor::visit(ReturnStatement& node)
+{
+    int id = get_node_id(&node);
+    m_dot_content << "  node" << id << " [label=\"ReturnStatement\"];\n";
+
+    if (node.expression) {
+        node.expression->accept(*this);
+        m_dot_content << "  node" << id << " -> node" << get_node_id(node.expression.get())
+                      << " [label=\"expression\"];\n";
+    }
+}
+
+void PrinterVisitor::visit(Function& node)
+{
+    int id = get_node_id(&node);
+    m_dot_content << "  node" << id << " [label=\"Function\"];\n";
+
+    if (node.name) {
+        node.name->accept(*this);
+        m_dot_content << "  node" << id << " -> node" << get_node_id(node.name.get())
+                      << " [label=\"name\"];\n";
+    }
+
+    if (node.body) {
+        node.body->accept(*this);
+        m_dot_content << "  node" << id << " -> node" << get_node_id(node.body.get())
+                      << " [label=\"body\"];\n";
+    }
+}
+
+void PrinterVisitor::visit(Program& node)
+{
+    int id = get_node_id(&node);
+    m_dot_content << "  node" << id << " [label=\"Program\", color=blue, style=filled, fillcolor=lightblue];\n";
+
+    if (node.function) {
+        node.function->accept(*this);
+        m_dot_content << "  node" << id << " -> node" << get_node_id(node.function.get())
+                      << " [label=\"function\"];\n";
+    }
+}
+
+int PrinterVisitor::get_node_id(const TackyAST* node)
+{
+    if (m_node_ids.find(node) == m_node_ids.end()) {
+        m_node_ids[node] = m_node_count++;
+    }
+    return m_node_ids[node];
+}
